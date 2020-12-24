0f056212bf7a3612f1b80c735efe945db900be07
TravlingCoder <travlingCoder@gmail.com>
2020-12-23 18:12:55 -0800

    Node_modules

---
+++ .sfdx/typings/lwc/apex/escalationOfPrivs.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/escalationOfPrivs.updateUser" {
+  export default function updateUser(param: {profileName: any}): Promise<any>;
+}
+declare module "@salesforce/apex/escalationOfPrivs.createUser" {
+  export default function createUser(param: {newUser: any}): Promise<any>;
+}
