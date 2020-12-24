3a6ca1d47d27f2352537259a9778e9fd698bd02c
TravlingCoder <travlingCoder@gmail.com>
2020-12-23 22:13:16 -0800

    Update parseResponse.py

--- S4cli/parseResponse.py
+++ S4cli/parseResponse.py
@@ -7,29 +7,29 @@ latest_file = max(list_of_files, key=os.path.getctime)
 findings = open(latest_file)
 
 scanFindings = json.load(findings)
-for i, x in scanFindings['scanSummary'].items():
-    if i == "Critical":
-        if x == 0:
-            print(i)
-            print(x)
+for severity, bugsFound in scanFindings['scanSummary'].items():
+    if severity == "Critical":
+        if bugsFound == 0:
+            print(severity)
+            print(bugsFound)
         else:
             exit("Too Many Critical Bugs")
-    if i == "High":
-        if x == 0:
-            print(i)
-            print(x)
+    if severity == "High":
+        if bugsFound == 0:
+            print(severity)
+            print(bugsFound)
         else:
             exit("Too Many High Bugs")
-    if i == "Medium":
-        if x == 0:
-            print(i)
-            print(x)
+    if severity == "Medium":
+        if bugsFound == 0:
+            print(severity)
+            print(bugsFound)
         else:
             exit("Too Many Medium Bugs")
-    if i == "Low":
-        if x > 0:
-            print(i)
-            print(x)
+    if severity == "Low":
+        if bugsFound == 0:
+            print(severity)
+            print(bugsFound)
         else:
             exit("Too Many Low Bugs")
 print(latest_file)
