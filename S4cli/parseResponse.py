9f256ba3e7692235d77cfef5a1729cfe8d8f890b
TravlingCoder <travlingCoder@gmail.com>
2020-12-23 21:38:37 -0800

    Create parseResponse.py

---
+++ S4cli/parseResponse.py
@@ -0,0 +1,24 @@
+import glob
+import os
+import json
+
+list_of_files = glob.glob('./*.json') # * means all if need specific format then *.csv
+latest_file = max(list_of_files, key=os.path.getctime)
+findings = open(latest_file)
+
+scanFindings = json.load(findings)
+for i, x in scanFindings['scanSumasdfasdfasdfasdmary'].items():
+    if i == "Critical":
+        print(i)
+        print(x)
+    if i == "High":
+        print(i)
+        print(x)
+    if i == "Medium":
+        print(i)
+        print(x)
+    if i == "Low":
+        print(i)
+        print(x)
+
+print(latest_file)
