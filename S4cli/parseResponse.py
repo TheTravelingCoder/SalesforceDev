919feb3bb63c752e80e4a713f28e12a45207ab7a
TravlingCoder <travlingCoder@gmail.com>
2020-12-23 22:20:55 -0800

    Update parseResponse.py

--- S4cli/parseResponse.py
+++ S4cli/parseResponse.py
@@ -9,24 +9,28 @@ findings = open(latest_file)
 scanFindings = json.load(findings)
 for severity, bugsFound in scanFindings['scanSummary'].items():
     if severity == "Critical":
+        #Change below to change amount of Critical bugs you are OK with
         if bugsFound == 0:
             print(severity)
             print(bugsFound)
         else:
             exit("S4 Found Too Many Critical Bugs, Exiting Build")
     if severity == "High":
+        #Change below to change amount of High bugs you are OK with
         if bugsFound == 0:
             print(severity)
             print(bugsFound)
         else:
             exit("S4 Found Too Many High Bugs, Exiting Build")
     if severity == "Medium":
+        #Change below to change amount of Medium bugs you are OK with
         if bugsFound == 0:
             print(severity)
             print(bugsFound)
         else:
             exit("S4 Found Too Many Medium Bugs, Exiting Build")
     if severity == "Low":
+        #Change below to change amount of Low bugs you are OK with
         if bugsFound == 0:
             print(severity)
             print(bugsFound)
