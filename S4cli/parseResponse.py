290bdc42ef8a1df1adda22e9af541b8a741c5e20
TravlingCoder <travlingCoder@gmail.com>
2021-01-08 12:51:29 -0800

    Update parseResponse.py

--- S4cli/parseResponse.py
+++ S4cli/parseResponse.py
@@ -2,10 +2,12 @@ import glob
 import os
 import json
 
+allFindings = {}
+
 list_of_files = glob.glob('./*.json') # * means all if need specific format then *.csv
 latest_file = max(list_of_files, key=os.path.getctime)
 findings = open(latest_file)
-
+print("JSON File downloaded: " + latest_file)
 scanFindings = json.load(findings)
 for severity, bugsFound in scanFindings['scanSummary'].items():
     if severity == "Critical":
@@ -14,26 +16,27 @@ for severity, bugsFound in scanFindings['scanSummary'].items():
             print(severity)
             print(bugsFound)
         else:
-            exit("S4 Found Too Many Critical Bugs, Exiting Build")
+            allFindings.update({severity: bugsFound})
     if severity == "High":
         #Change below to change amount of High bugs you are OK with
         if bugsFound == 0:
             print(severity)
             print(bugsFound)
         else:
-            exit("S4 Found Too Many High Bugs, Exiting Build")
+            allFindings.update({severity: bugsFound})
     if severity == "Medium":
         #Change below to change amount of Medium bugs you are OK with
         if bugsFound == 0:
             print(severity)
             print(bugsFound)
         else:
-            exit("S4 Found Too Many Medium Bugs, Exiting Build")
+            allFindings.update({severity: bugsFound})
     if severity == "Low":
         #Change below to change amount of Low bugs you are OK with
         if bugsFound == 0:
             print(severity)
             print(bugsFound)
         else:
-            exit("S4 Found Too Many Low Bugs, Exiting Build")
-print(latest_file)
+            allFindings.update({severity: bugsFound})
+if len(allFindings) > 0:
+    exit("S4 Found Too Many Bugs, Exiting Build")
